c57b582870752ec10249293b0cddd55f1d3ac8ca
ANKITs-Projects <ankitgupta0429@gmail.com>
2026-04-23 21:08:36 +0530

    Add an AIResponse component to structure the response out put

--- src/components/aiResponce/CodeBlock.jsx
+++ src/components/aiResponce/CodeBlock.jsx
@@ -1,7 +1,10 @@
-export default function CodeBlock({ code }) {
+export default function CodeBlock({ code, language }) {
   return (
-    <pre className="bg-black text-white p-3 rounded-lg overflow-x-auto text-sm">
-      <code>{code}</code>
-    </pre>
+    <div className="w-full flex flex-col gap-1">
+      <h3 className="text-lg font-black">{language}</h3>
+      <pre className="bg-black text-white p-3 rounded-lg overflow-x-auto text-sm">
+        <code>{code}</code>
+      </pre>
+    </div>
   );
 }
